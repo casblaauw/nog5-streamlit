fb4f0ce581f714cd50132404d7d7daa670de8638
Cas Blaauw <38132585+CasBlaauw@users.noreply.github.com>
2022-05-17 19:01:08 +0200

    Add training proteins, show AP score, randomize only val/test
    All predictions with model_region_2022-05-11-17-38_checkpoint-epoch53.pth.

--- explore_app.py
+++ explore_app.py
@@ -5,33 +5,41 @@ import matplotlib.pyplot as plt
 import sklearn.metrics as skm
 import random
 import pickle
+import zipfile
 
 # Read in data
-@st.cache
+@st.experimental_memo
 def read_data(path):
-  with open(path, 'rb') as f:
-    proteins = pickle.load(f)
-    protein_names = sorted(list(proteins.keys()))
+  with zipfile.ZipFile('model_region_2022-05-11-17-38_outputs.zip') as myzip:
+    with myzip.open('model_region_2022-05-11-17-38_outputs.pkl') as f:
+            proteins = pickle.load(f)
+            protein_names = sorted(list(proteins.keys()))
   return proteins, protein_names
+
+  # with open(path, 'rb') as f:
+  #   proteins = pickle.load(f)
+  #   protein_names = sorted(list(proteins.keys()))
+  # return proteins, protein_names
 
 
 if 'times_randomised' not in st.session_state:
     st.session_state['times_randomised'] = 0
 
-# path = 'G:\My Drive\Thesis\model_region_2022-05-07-22-59_outputs.pkl'
-path = 'model_region_2022-05-07-22-59_outputs.pkl'
+# path = 'G:\\My Drive\\Thesis\\model_region_2022-05-07-22-59_outputs_full.pkl'
+path = 'model_region_2022-05-11-17-38_outputs.pkl'
 prots, prot_names = read_data(path)
 
 
 # Basic layout
 
 # Get random protein
-@st.cache
+@st.experimental_memo
 def get_rand_id(change_to_refresh_value):
-  idx = random.choice(list(range(len(prot_names))))
+  valid_ids = [i for i, (k,v) in enumerate(sorted(prots.items())) if v['info']['split'] == 'Validation set' or v['info']['split'] == 'Test set']
+  idx = random.choice(valid_ids) #list(range(len(prot_names)))
   return idx
 
-if st.sidebar.button('Randomize protein'):
+if st.sidebar.button('Random test/validation set protein'):
   st.session_state['times_randomised'] += 1
 idx = get_rand_id(st.session_state['times_randomised'])
 
@@ -41,8 +49,8 @@ idx = prot_names.index(prot_name)
 
 
 # Shared items
-basic = st.sidebar.checkbox('Simplify plot?')
-show_stats = st.sidebar.checkbox('Show stats in title?', True)
+basic = st.sidebar.checkbox('Simplify plot')
+show_stats = st.sidebar.checkbox('Show stats in title', True)
 
 
 def plot_preds(preds, labels, idx = None, basic = False, show_stats = True):
@@ -64,7 +72,7 @@ def plot_preds(preds, labels, idx = None, basic = False, show_stats = True):
 
   title = ""
   if show_stats:
-    title += f" | F1: {skm.f1_score(y_true = labels, y_pred = decoded, zero_division=1)}, AUC: {skm.roc_auc_score(y_true = labels, y_score = probs[0, :, 1]) if np.sum(labels) > 0 else -1}"
+    title += f" | F1: {skm.f1_score(y_true = labels, y_pred = decoded, zero_division=1):0.5f}, AUC: {skm.roc_auc_score(y_true = labels, y_score = probs[0, :, 1]) if np.sum(labels) > 0 else -1:0.5f}, AP: {skm.average_precision_score(y_true = labels, y_score = probs[0, :, 1]) if np.sum(labels) > 0 else -1:0.5f}"
   if idx is not None:
     title = f"Entry {idx} " + title
   if title != "":
@@ -72,4 +80,5 @@ def plot_preds(preds, labels, idx = None, basic = False, show_stats = True):
   ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05), ncol = 3)
   st.pyplot(fig)
 
+st.title(f"{prot_name} | {prots[prot_name]['info']['split']}")
 plot = plot_preds(preds = prots[prot_name]['output'], labels = prots[prot_name]['input'], idx = prot_name, basic = basic, show_stats = show_stats)
